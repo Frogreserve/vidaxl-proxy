ede65e05011e9854f3b136dc3f98c89316126bf0
Frogreserve <frogreserve7@gmail.com>
2025-05-31 16:53:33 +0200

    Update get-products.php

--- get-products.php
+++ get-products.php
@@ -2,19 +2,19 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-// Zugangsdaten
+// vidaXL-Zugangsdaten
 $email = 'lichterae@gmail.com';
 $token = '6bf9794d-199d-4fa3-926a-3f94ac9620be';
 $auth = base64_encode("$email:$token");
 
-// API-Endpoint
-$url = 'https://b2b.vidaxl.com/api_customer/products?limit=30';
+// API-Endpunkt
+$url = 'https://b2b.vidaxl.com/api_customer/products';
 
+// Anfrage vorbereiten
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
-    "Authorization: Basic $auth",
-    "Accept: application/json"
+    "Authorization: Basic $auth"
 ]);
 
 $response = curl_exec($ch);
@@ -22,15 +22,10 @@ curl_close($ch);
 
 $data = json_decode($response, true);
 
-// Nur relevante Daten extrahieren
-$products = array_map(function ($item) {
-    return [
-        'id' => $item['id'] ?? '',
-        'name' => $item['name'] ?? '',
-        'price' => $item['price'] ?? '',
-        'currency' => $item['currency'] ?? '',
-        'image_url' => $item['main_image'] ?? ($item['images'][0] ?? null)
-    ];
-}, array_slice($data, 0, 30));
-
-echo json_encode($products);
+// Nur die ersten 50 Produkte zeigen
+if (isset($data['data'])) {
+    $limited = array_slice($data['data'], 0, 50);
+    echo json_encode($limited);
+} else {
+    echo json_encode(["error" => "Fehler beim Abrufen der Produkte"]);
+}
