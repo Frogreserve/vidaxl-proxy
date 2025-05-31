7a045d06ef0089c68016cd428a6e8342af282b69
Frogreserve <frogreserve7@gmail.com>
2025-05-31 17:51:53 +0200

    Update get-products.php

--- get-products.php
+++ get-products.php
@@ -1,14 +1,18 @@
+<?php
+// Wichtig: keine Leerzeichen oder Zeilen vor diesem <?php!
 
-<?php 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
+// Zugangsdaten zur vidaXL API
 $email = 'lichterae@gmail.com';
 $token = '6bf9794d-199d-4fa3-926a-3f94ac9620be';
 $auth = base64_encode("$email:$token");
 
+// API-Endpunkt
 $url = 'https://b2b.vidaxl.com/api_customer/products?limit=30';
 
+// Anfrage an vidaXL API senden
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, [
@@ -17,19 +21,3 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, [
 ]);
 
 $response = curl_exec($ch);
-curl_close($ch);
-
-$data = json_decode($response, true);
-
-// Produkte extrahieren
-$products = array_map(function ($item) {
-    return [
-        'id' => $item['id'] ?? '',
-        'name' => $item['name'] ?? '',
-        'price' => $item['price'] ?? '',
-        'currency' => $item['currency'] ?? '',
-        'image_url' => $item['main_image'] ?? null
-    ];
-}, array_slice($data, 0, 30));
-
-echo json_encode($products);
