afc8e3bdc3e040906da41c0460b232c7ba4240dd
Frogreserve <frogreserve7@gmail.com>
2025-06-22 15:15:11 +0200

    Update get-products-with-images.php

--- public/get-products-with-images.php
+++ public/get-products-with-images.php
@@ -6,7 +6,7 @@ $email = 'lichterae@gmail.com';
 $token = '6bf9794d-199d-4fa3-926a-3f94ac9620be';
 $auth = base64_encode("$email:$token");
 
-$url = 'https://b2b.vidaxl.com/api_customer/products?limit=50';
+$url = 'https://b2b.vidaxl.com/api_customer/products?limit=5';
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -16,23 +16,25 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, [
 ]);
 
 $response = curl_exec($ch);
+$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+$error = curl_error($ch);
 curl_close($ch);
 
-$data = json_decode($response, true);
-
-if (!is_array($data)) {
-    echo json_encode(['error' => 'Fehler bei API-Antwort']);
+// DEBUG: Falls Fehler
+if ($error || $httpCode !== 200) {
+    echo json_encode([
+        'error' => 'API Fehler',
+        'http_code' => $httpCode,
+        'curl_error' => $error,
+        'response' => $response
+    ]);
     exit;
 }
 
-$products = array_map(function ($item) {
-    return [
-        'id' => $item['id'] ?? '',
-        'name' => $item['name'] ?? '',
-        'price' => $item['price'] ?? '',
-        'currency' => $item['currency'] ?? '',
-        'image_url' => $item['main_image'] ?? null
-    ];
-}, $data);
+// Versuche JSON zu dekodieren
+$data = json_decode($response, true);
 
-echo json_encode($products);
+echo json_encode([
+    'debug' => 'Antwort erfolgreich empfangen',
+    'original' => $data
+]);
